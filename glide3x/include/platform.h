b87f9351e23c544c430b020c856ff86bb3546a6b
OneMeanDragon <17592330+OneMeanDragon@users.noreply.github.com>
2024-09-23 23:58:37 -0400

    Add files via upload

--- glide3x/include/platform.h
+++ glide3x/include/platform.h
@@ -1,6 +1,12 @@
 #pragma once
 #define NOMINMAX /* limits */
 
+/*
+	being as the dll has to be __stdcall you'll need the .def file
+	if this was cdecl the .def file wouldent be needed, so instead
+	of using the following were actually useing whats in the sdk 
+	itself.
+*/
 #define EXTERNC extern "C"
 
 #if defined(_WIN32)
@@ -12,6 +18,11 @@
 	#define IMPORT
 #endif
 
+/*
+	all of the folders required for the 3dfx sdk
+	are in the project propertys under vc++ 
+	directorys
+*/
 #include <swlibs/fxmisc/3dfx.h>
 #include <h5/glide3/src/glide.h>
 
